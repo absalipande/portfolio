7dd08a9cfd5d9689d607c84d9f04ee5ddbddcdd0
Amiel Brencis Salipande <absalipande@gmail.com>
2022-12-23 17:12:52 +0800

    small changes in About Me section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,28 +4,28 @@ import coding from '../image/coding.png';
 
 function About() {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-center gap-10 md:gap-20">
-      <div className="w-full md-w-6/12">
+    <div className='flex flex-col md:flex-row items-center justify-center gap-10 md:gap-20'>
+      <div className='w-full md-w-6/12'>
         <Title>
-          <span className="flex items-center justify-center">About Me</span>
+          <span className='flex items-center justify-center'>About Me</span>
         </Title>
-        <p className="text-center text-md text-black dark:text-white">
+        <p className='text-center text-md text-black dark:text-white'>
           I am an aspiring Software Developer with a deep passion for developing
           innovative software that will help an organization’s efficiency and
-          effectiveness. <br />I have trained and have the necessary skills to
-          become a Software Developer. <br />
+          effectiveness. <br />
+          <br />
           Growth and progress are of utmost importance to me; hence,
           continuously upskilling myself to meet the industry’s demands and
-          standards and write reusable, user-friendly code. <br />A young mind
-          with sharp focus and willingness to learn to develop software to meet
-          an organization’s needs, highlights its core competencies, and further
-          its success.
+          standards and write reusable, user-friendly code. <br /> As a
+          motivated and focused individual with a strong desire to learn, I am
+          confident in my ability to develop software solutions that enhances an
+          organization’s core competencies and contribute to its success.
         </p>
       </div>
       <img
         src={coding}
-        alt="codingSample"
-        className="w-full md:w-6/12 rounded-lg object-cover"
+        alt='codingSample'
+        className='w-full md:w-6/12 rounded-lg object-cover'
       />
     </div>
   );
